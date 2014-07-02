92262ec44936849dd4770d034e9fae32eaf3a3b2
theburningmonk <theburningmonk@gmail.com>
2014-07-02 22:50:23 +0100

    - added scrubber for sentry key and secret

--- raven_dart/lib/src/scrubber.dart
+++ raven_dart/lib/src/scrubber.dart
@@ -4,12 +4,13 @@ abstract class Scrubber {
   String scrub(String input);
 }
 
-/**
- * Scrubber to remove credit card numbers from the input string
- */
-class CreditCardScrubber implements Scrubber {
-  // see http://www.regular-expressions.info/creditcard.html for the pattern used here
-  static final _regex   = new RegExp(r"\b(?:\d[ -]*?){13,16}\b", multiLine: true, caseSensitive: false);
+abstract class RegexScrubber implements Scrubber {
+  final _regex;
+
+  RegexScrubber(String pattern):
+    this._regex = new RegExp(pattern, multiLine: true, caseSensitive: false);
+
+  String replace(String match);
 
   String scrub(String input) {
     String output = input;
@@ -18,13 +19,37 @@ class CreditCardScrubber implements Scrubber {
     // for any matched credit card numbers, replace it
     for (Match m in matches) {
       String match = m.group(0);
-      output = output.replaceAll(match, "####-CC-TRUNCATED-####");
+      output = output.replaceAll(match, replace(match));
     }
 
     return output;
   }
 }
 
-class _DefaultScrubber implements Scrubber {
-  String scrub(String input) => "";
+/**
+ * Scrubber to remove credit card numbers from the input string
+ */
+class CreditCardScrubber extends RegexScrubber {
+  // see http://www.regular-expressions.info/creditcard.html for the pattern used here
+  CreditCardScrubber() : super(r"\b(?:\d[ -]*?){13,16}\b");
+
+  replace(_) => "####-CC-TRUNCATED-####";
+}
+
+/**
+ * Scrubber to remove Sentry key header from the input string
+ */
+class SentryKeyScrubber extends RegexScrubber {
+  SentryKeyScrubber() : super(r"sentry_key[ ]*[:=-][ ]*[0-9a-z-A-Z]+");
+
+  replace(_) => "####-SENTRY-KEY-TRUNCATED-####";
+}
+
+/**
+ * Scrubber to remove Sentry secret header from the input string
+ */
+class SentrySecretScrubber extends RegexScrubber {
+  SentrySecretScrubber() : super(r"sentry_secret[ ]*[:=-][ ]*[0-9a-z-A-Z]+");
+
+  replace(_) => "####-SENTRY-SECRET-TRUNCATED-####";
 }
--- raven_dart/test/raven_dart_test.dart
+++ raven_dart/test/raven_dart_test.dart
@@ -4,13 +4,15 @@ import 'src/dsn_parser_test.dart';
 import 'src/message_test.dart';
 import 'src/raven_client_test.dart';
 import 'src/utils_test.dart';
-import 'src/scrubbers/credit_card_test.dart';
+import 'src/scrubbers_test.dart';
 
 main() {
   new DsnParserTests().start();
   new MessageTests().start();
   new RavenClientTests().start();
   new UtilsTests().start();
-  
+
   new CreditCardScrubberTests().start();
+  new SentryKeyScrubberTests().start();
+  new SentrySecretScrubberTests().start();
 }
