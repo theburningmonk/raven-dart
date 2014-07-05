c13574ec0959f9b3d32a22141b328db364578081
theburningmonk <theburningmonk@gmail.com>
2014-07-05 01:45:09 +0100

    - added comments

--- raven_dart/lib/raven_dart.dart
+++ raven_dart/lib/raven_dart.dart
@@ -18,7 +18,24 @@ class RavenClient {
   final List<Scrubber> _scrubbers;
   HttpRequester _requester;
 
-  // Note: since the isolates are doing mostly IO work, it makes sense to have more isolate than cores
+  /**
+   * You need to specify the [dsn] for your project in Sentry.
+   *
+   * If there is a set of common tags that should be applied to ALL your events (both exceptions and non-exceptions)
+   * then you should provide them via the optional parameter [tags].
+   * If [tags] include keys that are overlapped by event specific tags provided in the [captureException] and [captureMessage]
+   * methods then both tags are sent in the event.
+   *
+   * Additionally, you can specify the max level of concurrent requests to Sentry per available processor on the current system.
+   * Since sending HTTP requests is not CPU intensive work, so the default [lvlOfConcurrencyPerCore] is set to 3 which shouldn't
+   * ever stop you from being able to do other useful work on the CPUs but should you need to you can dial that up and down to
+   * suit your needs.
+   *
+   * Similarly, failed HTTP requests that make sense to retry (e.g. temporary network issue, etc.) are retried up to a maximum
+   * of 3 times, but you can change that number by setting the [maxRetries] optional parameter. A minimum wait of 100ms is used
+   * between retries, with the delay increasing exponentially as the number of retries increases.
+   *
+   */
   RavenClient(String dsn,
               { Map<String, String> tags,
                 List<Scrubber> scrubbers,
@@ -36,6 +53,34 @@ class RavenClient {
                                      maxRetries              : maxRetries);
     }
 
+  /**
+   * Captures information about an exception/error as an event in Sentry.
+   *
+   * ## Arguments
+   * [exn]        - the exception/error object.
+   * [stackTrace] - the StackTrace captured along with [exn].
+   * [logger]     - the name of the logger, defaults to 'root'.
+   * [logLevel]   - the severity level of this exception, defaults to [LogLevel.ERROR].
+   * [tags]       - a map of custom tags to be logged along with the exception information.
+   * [extra]      - a map of additional metadata to store with the event.
+   *
+   * ## Example
+   * In order to capture the minimum amount of information with an exception:
+   *
+   *      import 'package:raven_dart/raven_dart.dart';
+   *
+   *      main() {
+   *        var dsn    = 'https://b70a31b3510c4cf793964a185cfe1fd0:b7d80b520139450f903720eb7991bf3d@example.com/1';
+   *        var client = new RavenClient(dsn);
+   *
+   *        try {
+   *          throw new Exception("test exception");
+   *        } catch (exn, stackTrace) {
+   *          client.captureException(exn, st);
+   *        }
+   *      }
+   *
+   */
   void captureException(exn,
                         StackTrace stackTrace,
                         { String logger : _defaultLogger,
@@ -54,6 +99,27 @@ class RavenClient {
     _requester.sendMessage(sentryMsg);
   }
 
+  /**
+   * Captures an arbitrary message as an event in Sentry.
+   *
+   * ## Arguments
+   * [message]    - the message to be recorded.
+   * [logger]     - the name of the logger, defaults to 'root'.
+   * [logLevel]   - the severity level of this exception, defaults to [LogLevel.INFO].
+   * [tags]       - a map of custom tags to be logged along with the message.
+   * [extra]      - a map of additional metadata to store with the event.
+   *
+   * ## Example
+   *
+   *      import 'package:raven_dart/raven_dart.dart';
+   *
+   *      main() {
+   *        var dsn    = 'https://b70a31b3510c4cf793964a185cfe1fd0:b7d80b520139450f903720eb7991bf3d@example.com/1';
+   *        var client = new RavenClient(dsn);
+   *        client.captureMessage("test");
+   *      }
+   *
+   */
   void captureMessage(String message,
                       { String logger : _defaultLogger,
                         LogLevel logLevel : LogLevel.INFO,
@@ -68,5 +134,8 @@ class RavenClient {
     _requester.sendMessage(sentryMsg);
   }
 
+  /**
+   * Gracefully disposes of the client and associated resources.
+   */
   void close() => _requester.close();
 }
